34339e06eb99328f7c8f73dc0c8ea2bced1a3ca2
Lionel E. Ramos <ltrainpr@gmail.com>
2015-07-18 10:44:35 -0600

    WIP: Took out document.ready that was re-rendering Tasks

--- app/assets/javascripts/components/tasks.js.jsx
+++ app/assets/javascripts/components/tasks.js.jsx
@@ -1,21 +1,39 @@
-"use strict";
+(function(){
+  "use strict";
+
+  var List = React.createClass({
+    renderItems: function() {
+      return _.map(this.props.tasks, function(task) {
+        return <li className="list-group-item pointer" >{task.name}</li>;
+      });
+    },
+
+    render: function() {
+      return  <ul className="list-group">
+                {this.renderItems()}
+              </ul>;
+      }
+  });
+
+  var Tasks = React.createClass({
 
 
-var Tasks = React.createClass({
-  renderList: function(complete){
-    console.log(_.filter(this.props.data, function(x) { return x.complete === complete; }));
-    return <ul className="list-group"></ul>
-  },
+    renderList: function(complete){
+      return <List tasks={_.filter(this.props.data, function(x) { return x.complete === complete; })} />;
+    },
 
-  render: function() {
-    return  <div className="col-md-6">
-              <h1>Todo List</h1>
-              <h2 className="spacing-bottom">Incomplete</h2>
-              {this.renderList(false)}
-            </div>;
-  }
-});
-
-$(document).ready(function () {
-  React.render(<Tasks data={this.props.data} />, document.getElementById('tasks'));
-})
+    render: function() {
+      return  <div className="todos">
+                <div className="incomplete-todo">
+                  <h1>Todo List</h1>
+                  <h2 className="spacing-bottom">Incomplete</h2>
+                  {this.renderList(false)}
+                </div>
+                <div className="complete-todo">
+                  <h2 className="spacing-bottom">Complete</h2>
+                  {this.renderList(true)}
+                </div>
+              </div>
+    }
+  });
+})();
